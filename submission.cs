a6f77f8f0ec8adb44b31c777a8717ce06c9e9590
Kseniia Piskun <78762092+kseniia-pskn@users.noreply.github.com>
2024-11-03 22:23:55 +0200

    Add files via upload

--- submission.cs
+++ submission.cs
@@ -2,16 +2,15 @@ using System;
 using System.Xml;
 using System.Xml.Schema;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace ConsoleApp1
 {
     public class Program
     {
-        // URLs to be updated with your ASU web-hosted files
-        public static string xmlURL = "https://www.public.asu.edu/kpiskun/Hotels.xml"; //Q1.2
-        public static string xmlErrorURL = "https://www.public.asu.edu/kpiskun/HotelsErrors.xml"; //Q1.3
-        public static string xsdURL = "https://www.public.asu.edu/kpiskun/Hotels.xsd"; //Q1.1
+        // Local file paths for macOS
+        public static string xmlURL = "/Users/kseniia.piskun/Desktop/Assignment4_XMLProject/Hotels.xml";
+        public static string xmlErrorURL = "/Users/kseniia.piskun/Desktop/Assignment4_XMLProject/HotelsErrors.xml";
+        public static string xsdURL = "/Users/kseniia.piskun/Desktop/Assignment4_XMLProject/Hotels.xsd";
 
         public static void Main(string[] args)
         {
@@ -28,22 +27,23 @@ namespace ConsoleApp1
             Console.WriteLine("JSON Output (Hotels.xml): " + result);
         }
 
-        public static string Verification(string xmlUrl, string xsdUrl)
+        public static string Verification(string xmlFilePath, string xsdFilePath)
         {
             try
             {
                 XmlSchemaSet schema = new XmlSchemaSet();
-                schema.Add("", xsdUrl);
+                schema.Add("", xsdFilePath);
 
                 XmlReaderSettings settings = new XmlReaderSettings();
-                settings.Schemas.Add(schema);
+                settings.Schemas = schema;
                 settings.ValidationType = ValidationType.Schema;
+
                 settings.ValidationEventHandler += (sender, e) =>
                 {
                     throw new Exception(e.Message);
                 };
 
-                using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
+                using (XmlReader reader = XmlReader.Create(xmlFilePath, settings))
                 {
                     while (reader.Read()) { }
                 }
@@ -56,22 +56,21 @@ namespace ConsoleApp1
             }
         }
 
-        public static string Xml2Json(string xmlUrl)
+        public static string Xml2Json(string xmlFilePath)
         {
             XmlDocument doc = new XmlDocument();
-            doc.Load(xmlUrl);
+            doc.Load(xmlFilePath);
 
+            // Convert XML to JSON string using Newtonsoft.Json
             string jsonText = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented, true);
-            JObject jsonObject = JObject.Parse(jsonText);
 
-            // Optional: Remove empty fields like "_Rating" if not present in XML
-            jsonObject.Descendants()
-                .OfType<JProperty>()
-                .Where(attr => attr.Name.StartsWith("_") && attr.Value.Type == JTokenType.Null)
-                .ToList()
-                .ForEach(attr => attr.Remove());
+            // Manually remove empty "Rating" fields if necessary (JSON.NET does not handle this in .NET 4.5)
+            if (jsonText.Contains("\"_Rating\": null"))
+            {
+                jsonText = jsonText.Replace("\"_Rating\": null,", "");
+            }
 
-            return jsonObject.ToString();
+            return jsonText;
         }
     }
 }
